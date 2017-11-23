4e13556725268dbb183f82bc6524e8d1f7e85597
renztp <tullaorenz@gmail.com>
2017-11-23 16:02:33 +0800

    First Commit

---
+++ renz_documentations/javascript/js_tests/script.js
@@ -0,0 +1,36 @@
+// Javascript documentation
+
+// COMMENTS
+
+// this is a single line comment
+/* 
+*
+* This is a multi line comment
+*
+*/
+
+// DECLARING A VARIABLE IN JAVASCRIPT
+// Javascript is not statically typed
+var a = 25; // a number variable
+var name = 'Renz' // a stringed variable
+
+// OPERATORS
+// You can do basic arithmetic in javascript
+// and can use variables to manipulate data
+
+var a = 20;
+var b = 30;
+var result = a + b; // (a)20 + (b)30 = result
+console.log(result); // result = 50
+
+// Addition
+var result = a + b; // result = 50
+// Subtraction
+var result = a - b; // result = -20
+// Division
+var result = a / b; // result = 0.66
+// Multiplication
+var result = a * b; // result = 600 
+
+
+
